0f898e1561a16ac862de2953490dfc47ec1296f7
daniel-davee <daniel.v.davee@gmail.com>
2022-12-10 00:13:32 -0700

    Add in keyword inputs for openai.Completion.create

--- cli_companion.py
+++ cli_companion.py
@@ -2,20 +2,28 @@ from plac import call
 from os import environ
 from InquirerPy.inquirer import text
 import openai
+from typing import Optional
 
-def main():
+def main(
+        prompt: ('Your propmt','positional')='',
+        engine: (
+                    'The engine you use davinci|curie|ada',
+                    'option','e',
+                 )='davinci',
+        max_tokens:('max tokens used in response','option','max')=1024,
+        n: ('The number of generated','option')=1,
+        temperature: ('1 for more random','option','t') = 0.5,
+                                    ):
     if 'CHATKEY' not in environ: raise Exception('Set CHARKEY')
     openai.api_key = environ['CHATKEY']
 
-    prompt = text('What do you want to ask?').execute()
-    print(prompt)
     completions = openai.Completion.create(
-        engine="text-davinci-002",
-        prompt=prompt,
-        max_tokens=1024,
-        n=1,
+        engine=f"text-{engine}-{'002' if engine == 'davinci' else '001'}",
+        prompt=prompt or text('What do you want to ask?').execute(),
+        max_tokens=max_tokens,
+        n=n,
         stop=None,
-        temperature=0.5,
+        temperature=temperature,
     )
 
     generated_text = completions.choices[0].text
