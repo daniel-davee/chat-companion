b515b0812ce8ee369f951afe561094a41144a36e
daniel-davee <daniel.v.davee@gmail.com>
2022-12-11 15:36:37 -0700

    added the abiliety to save output to a file

--- companion.py
+++ companion.py
@@ -1,4 +1,5 @@
-from plac import call, Interpreter
+from plac import Interpreter
+from pathlib import Path
 from os import environ
 from InquirerPy.inquirer import text, fuzzy
 from toolbox.companion_logger import logger
@@ -17,10 +18,13 @@ class Companion(object):
             max_tokens:('max tokens used in response','option','max')=1024,
             n: ('The number of generated','option')=1,
             temperature: ('1 for more random','option','t') = 0.5,
+                filename:('''The file name to output review
+                            for example scrach.py
+                        ''','option','f')='',
+                                        ):
         '''
         Allows you to talk chat gpt
         '''
-                                        ):
         if 'CHATKEY' not in environ: raise Exception('Set CHARKEY')
         openai.api_key = environ['CHATKEY']
 
@@ -39,8 +43,14 @@ class Companion(object):
         logger.response(generated_text)
         with shelve.open('.history') as hst:
             hst['history'] |= {prompt:generated_text}
+        
+        if filename:(Path()/filename).write_text(generated_text)
    
-    def review(self):
+    def review(self,
+               filename:('''The file name to output review
+                            for example scrach.py
+                         ''','option','f')='',
+               ):
         '''
         To review previous questions and responses,
         use the `review` subcommand. This will bring up a list of previous questions.
@@ -51,7 +61,8 @@ class Companion(object):
                             choices=list(hst['history'].keys()),
                             vi_mode=True,
                             ).execute()
-            print(hst['history'][prompt])
+            print((response:=hst['history'][prompt]))
+        if filename:(Path()/filename).write_text(response)
        
 if __name__ == '__main__':
     Interpreter.call(Companion)   
