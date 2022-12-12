ff5af89ccbd62b4a482847d6f48ecc21e8c2e1cf
daniel-davee <daniel.v.davee@gmail.com>
2022-12-11 23:44:06 -0700

    fixed path problem

--- companion.py
+++ companion.py
@@ -6,11 +6,14 @@ from toolbox.companion_logger import logger
 import openai
 import shelve
 
-cwd = Path(__file__).parent
 
 class Companion(object):
-    commands = 'talk', 'review'
+    commands = [
+                # 'talk', 
+                'review',
+                ]
     
+    cwd = Path().cwd()
     def talk(self,
             prompt: ('Your propmt','positional')='',
             engine: (
@@ -58,6 +61,8 @@ class Companion(object):
         use the `review` subcommand. This will bring up a list of previous questions.
         You can then select a question to view the response.
         '''
+        print(Path().cwd().name)
+        print((cwd:=self.cwd))
         with shelve.open(str(cwd/'.history')) as hst:
             prompt = fuzzy('What prompt do you want to review', 
                             choices=list(hst['history'].keys()),
@@ -65,6 +70,19 @@ class Companion(object):
                             ).execute()
             print((response:=hst['history'][prompt]))
         if filename:(Path()/filename).write_text(response)
+    
+    @property       
+    def conversations(self):
+        with shelve.open(str(cwd/'.history')) as hst:
+            txt = [[l for l in t.split('\n') if l.strip() and l!='?'] 
+            for t in f.read().split('##########\n')]
+            txt = [t for t in txt if t]
+            response = [t[-1] for t in txt if 'RESPONSE' in t[0]]
+            prompts = [t[-1] for t in txt if 'PROMPT' in t[0]]
+            conversations = {p:r for p,r in zip(prompts,response)}
+        with shelve.open('.history') as hst:
+            hst['history'] = conversations
+        return conversations
        
 if __name__ == '__main__':
     Interpreter.call(Companion)   
